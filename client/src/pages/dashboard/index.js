8d58057fcdded995366db7654ba1be2fd009f982
Anthony <adavid16967@gmail.com>
2023-05-25 09:02:53 -0400

    added styling for listing card compo

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,15 +1,22 @@
-import { useState } from "react"
-import ListingCard from '../../components/listing-card/ListingCard.tsx'
-import SearchBar from '../../components/search/SearchBar.tsx'
+import { useState } from "react";
+import ListingCard from "../../components/listing-card/ListingCard";
+import SearchBar from "../../components/search/SearchBar";
 
-export default function Login() {
-	
-	
+export default function Dashboard() {
+  const [data, setData] = useState([1, 2, 3, 4, 5, 6]);
 
-	return (
-		<div className="mainContainer">
-			<SearchBar />
-			<ListingCard />
-		</div>	
-	)
+  return (
+    <div className="container mx-auto py-8">
+			<div class="mb-6">
+      <SearchBar />
+			</div>
+      <div className="flex flex-wrap justify-center -mx-4">
+        {data.map((item) => (
+          <div key={item} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-2 mb-4">
+            <ListingCard />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
