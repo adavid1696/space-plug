438cfff69cca750260e1895db213490595c952c1
Anthony <adavid16967@gmail.com>
2023-05-24 20:29:45 -0400

    fixed auth

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -2,17 +2,17 @@
 import User from '../model/userModel.js'
 
 export const login = async (req, res, next) => {
-
-	const { email, password } = req.body;
 	
+	const { email, password } = req.body;
+
 	try {
 		const user = await User.findOne({email, password});
 
 		if(!user){
-			res.status(500).json('Incorrect email or password! Try again.')
+			res.status(404).json('Incorrect email or password! Try again.')
 		}
 
-		res.status(200).json("Succesfull Login!")
+		else res.status(200).json("Succesfull Login!")
 	} catch (e) {
 		next(e)
 	}
