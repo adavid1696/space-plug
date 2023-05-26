04b86a6f4d83c2426c80aaef6c4ab2f3e9c000b8
Anthony <adavid16967@gmail.com>
2023-05-26 13:42:46 -0400

    added domains for diff imgs

--- client/src/components/listing-card/ListingCard.tsx
+++ client/src/components/listing-card/ListingCard.tsx
@@ -1,29 +1,47 @@
 import React from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-const DetailsCard = () => {
+interface ListingCardProps {
+  item: {
+    _id: string,
+    title: string,
+    price: number,
+  }
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({ item }) => {
+
+  const router = useRouter()
+
+  const handleClick = () => {
+    const serializedItem = encodeURIComponent(JSON.stringify(item))
+    router.push({
+      pathname: `/listing/${item._id}}`,
+      query: {data: serializedItem}
+    })
+  }
+
   return (
-    <div className="bg-white rounded-lg shadow-md p-4">
-      <div className="relative h-48 w-full mb-4">
+    
+    <div onClick={handleClick} className="bg-white rounded-lg shadow-md p-3">
+      <div className="relative h-48 w-full mb-2">
         <Image
-          src="/path/to/image.jpg"
+          src="https://media.timeout.com/images/105813983/1920/1080/image.jpg"
           alt="Image Alt Text"
           width={500}
           height={300}
           className="rounded-lg"
         />
       </div>
-      <h3 className="text-xl font-semibold mb-2">Card Title</h3>
-      <p className="text-gray-600 mb-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies
-        massa non arcu tristique, vel tincidunt neque consectetur. Vivamus
-        fringilla tortor id lectus iaculis feugiat.
-      </p>
-      <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md">
+      <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
+      <p>Starting price at ${item.price}/hr</p>
+      
+      {/* <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md">
         Read More
-      </button>
+      </button> */}
     </div>
   );
 };
 
-export default DetailsCard;
+export default ListingCard;
