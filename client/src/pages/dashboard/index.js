5ba24a5a5a1841768d1f34a7ae7c5cb3b6b89fa8
Anthony <adavid16967@gmail.com>
2023-05-26 13:44:28 -0400

    added some changes

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,19 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ListingCard from "../../components/listing-card/ListingCard";
 import SearchBar from "../../components/search/SearchBar";
+import { useRouter } from "next/router";
 
 export default function Dashboard() {
-  const [data, setData] = useState([1, 2, 3, 4, 5, 6]);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const res = await fetch('http://localhost:3001/item');
+      const resJSON = await res.json();
+
+      setData(resJSON)
+    }
+
+    fetchAPI()
+
+  }, [])
+  
+  if(!data) return 'Loading Data'
 
   return (
+    
     <div className="container mx-auto py-8">
-			<div class="mb-6">
-      <SearchBar />
+			<div className="mb-6">
+      	<SearchBar />
 			</div>
       <div className="flex flex-wrap justify-center -mx-4">
         {data.map((item) => (
-          <div key={item} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-2 mb-4">
-            <ListingCard />
+          <div key={item._id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-2 mb-4">
+            <ListingCard item={item}/>
           </div>
         ))}
       </div>
