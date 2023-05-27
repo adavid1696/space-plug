acb97cd97b92aa0c87428ec82fc8e6da5361f6fe
Anthony <adavid16967@gmail.com>
2023-05-27 08:50:29 -0400

    fixed typescript error

--- client/src/pages/listing/[id].tsx
+++ client/src/pages/listing/[id].tsx
@@ -11,11 +11,13 @@ interface Item {
   price: number;
   startTime: string;
   endTime: string;
-	user: string
+  user: string;
 }
 
 interface Comment {
-	content: string
+  content: string,
+  _id: string,
+  user: string
 }
 
 interface CustomRouterQuery {
@@ -24,48 +26,43 @@ interface CustomRouterQuery {
 
 export default function ListingPage() {
   const router = useRouter();
-	const [comments, setComments] = useState<string[]>([])
+  const [comments, setComments] = useState<Comment[]>([]);
 
   const { data }: CustomRouterQuery = router.query;
-  const item : Item = data ? JSON.parse(decodeURIComponent(data)) : null;
+  const item: Item | null = data ? JSON.parse(decodeURIComponent(data)) : null;
 
-	useEffect(() => {
-		if(item){
+  useEffect(() => {
+    if (item) {
+      const fetchAPI = async () => {
+        const res = await fetch(`http://localhost:3001/item/${item._id}/comments`);
+        const resJSON = await res.json();
+        setComments(resJSON);
+      };
+      fetchAPI();
+    }
+  }, [item]);
 
-			const fetchAPI = async () => {
-				const res = await fetch(`http://localhost:3001/item/${item._id}/comments`)
-				const resJSON = await res.json();
-				setComments(resJSON)
-			}
-			fetchAPI()	
-		}
-	
-	
-		
-	}, [item])
-	
-	if(!comments || !item) return "Loading Data"
+  if (!comments || !item) return "Loading Data";
 
   return (
-		<div className="container">
-			<div className="imgContainer">
-				<Image 
-					src="https://media.timeout.com/images/105813983/1920/1080/image.jpg"
-      	  alt="Image Alt Text"
-      	  width={600}
-      	  height={300}/>
-			</div>
-			<h3 className="title">{item.title}</h3>
-			<h4>{item.location}</h4>
-			<div className="hostInfo">
-				<h3>{item.user}</h3>
-				<button>Message</button>
-			</div>
-			<div className="comments">
-				{comments && comments?.map(comment => (
-					<p>{comment.content}</p>
-				))}
-			</div>
-		</div>
-	)
+    <div className="container mx-auto px-4 py-8">
+      <div className="imgContainer">
+        <Image src="https://media.timeout.com/images/105813983/1920/1080/image.jpg" alt="Image Alt Text" width={600} height={300} />
+      </div>
+      <h3 className="text-3xl font-bold mt-4">{item.title}</h3>
+      <h4 className="text-lg text-gray-500 mb-4">{item.location}</h4>
+      <div className="hostInfo flex items-center justify-between mb-4">
+        <h3 className="text-xl">{item.user}</h3>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Message</button>
+      </div>
+      <div className="comments">
+        {comments.map((comment) => (
+					<>
+						{comment.user}
+          	<p key={comment._id} className="mb-2">{comment.content}</p>
+					</>
+        ))}
+      </div>
+    </div>
+  );
 }
