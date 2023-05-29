534aeb9df035a7c9230a5fe927ac1f260bd272ff
Anthony <adavid16967@gmail.com>
2023-05-29 18:14:30 -0400

    added ssr for comments

--- client/src/pages/listing/[id].tsx
+++ client/src/pages/listing/[id].tsx
@@ -24,9 +24,21 @@ interface CustomRouterQuery {
   data?: string;
 }
 
-export default function ListingPage() {
+export async function getServerSideProps(context) {
+  const { query } = context;
+
+  const res = await fetch(`http://localhost:3001/item/${query.id}/comments`);
+  const comments: Comment[] = await res.json();
+  return {
+    props: {
+      comments,
+    },
+  };
+
+}
+
+export default function ListingPage({ comments } ) {
   const router = useRouter();
-  const [comments, setComments] = useState<Comment[]>([]);
   const [currentImg, setCurrentImg] = useState(0)
 
   const { data }: CustomRouterQuery = router.query;
@@ -41,25 +53,10 @@ export default function ListingPage() {
     'https://www.therooftopguide.com/rooftop-bars-in-new-york/Bilder/dear-irving-on-hudson-600-6.jpg',
   ]
 
-  useEffect(() => {
-    if (item) {
-      const fetchAPI = async () => {
-        const res = await fetch(`http://localhost:3001/item/${item._id}/comments`);
-        const resJSON = await res.json();
-        setComments(resJSON);
-      };
-      fetchAPI();
-    }
-  }, [item]);
-
-  if (!comments || !item) return "Loading Data";
-
   const handleNext = (e: any) => {
     e.preventDefault();
     
-    
     if(!imgs[currentImg + 1]){
-      console.log(currentImg)
       setCurrentImg(0)
     }else setCurrentImg(prev => prev + 1)
   }
@@ -102,15 +99,15 @@ export default function ListingPage() {
       <h3 className="text-3xl font-bold mt-4">{item.title}</h3>
       <h4 className="text-lg text-gray-500 mb-4">{item.location}</h4>
       <div className="hostInfo flex items-center justify-between mb-4">
-        <h3 className="text-xl">{item.user}</h3>
+        <h3 className="text-xl">{item.user.firstName} {item.user.lastName}</h3>
         <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Message</button>
       </div>
       <div className="comments">
         {comments.map((comment) => (
-					<>
+					<div key={comment._id} >
 						{comment.user}
-          	<p key={comment._id} className="mb-2">{comment.content}</p>
-					</>
+          	<p className="mb-2">{comment.content}</p>
+					</div>
         ))}
       </div>
     </div>
